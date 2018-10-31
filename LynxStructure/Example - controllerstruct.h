9773aabb916e7803a572139a795c4c2c7dcf1dcb
Teodor-NA <44146323+Teodor-NA@users.noreply.github.com>
2018-10-31 23:52:39 +0100

    Add files via upload

--- LynxStructure/Example - controllerstruct.h
+++ LynxStructure/Example - controllerstruct.h
@@ -1,31 +1,38 @@
-#ifndef CONTROLLERSTRUCT_H
-#define CONTROLLERSTRUCT_H
+#pragma once
 #include "LynxStructure.h"
+
+#define CONTROLLER_STRUCT 0x12
 
 using namespace LynxStructureSpace;
 
 enum ControllerContents
 {
-    joy_LX,
-    joy_LY,
-    joy_RX,
-    joy_RY,
-    trigger_L,
-    trigger_R,
-    buttons,
-    endOfList
+	joy_LX,
+	joy_LY,
+	joy_RX,
+	joy_RY,
+	trigger_L,
+	trigger_R,
+	buttons,
+	endOfList
 };
 
-LynxStructureSpace::LynxStructure::InitParam initParamsController[8]
-    {
-        { joy_LX, eInt16 },
-        { joy_LY, eInt16 },
-        { joy_RX, eInt16 },
-        { joy_RY, eInt16 },
-        { trigger_L, eInt8 },
-        { trigger_R, eInt8 },
-        { buttons, eUint32 },
-        { endOfList, eEndOfList }
-    };
-#endif // CONTROLLERSTRUCT_H
+static const StructItem controllerItems[]
+{
+	{ joy_LX, eInt16 },
+	{ joy_LY, eInt16 },
+	{ joy_RX, eInt16 },
+	{ joy_RY, eInt16 },
+	{ trigger_L, eInt8 },
+	{ trigger_R, eInt8 },
+	{ buttons, eUint32},
+	{ endOfList, eEndOfList }
+};
 
+static const StructDefinition controllerDefinition
+{
+	{"Controller Struct"},
+	{ controllerItems }
+};
+
+// static const char controllerName[] = "Controller Struct";
